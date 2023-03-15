10f1a7122b0de75b87767e798732eec2353c6b5d
flynndelany <flynndelany@gmail.com>
2023-03-15 12:24:59 -0400

    The actual boxplot

--- Epifauna.R
+++ Epifauna.R
@@ -1,4 +1,16 @@
+library(tidyverse)
+
 ## Wet Weight
 
-WW <- read.csv("D:/Projects/Blocks/Data/Epifauna_Wet.csv")
-summary(WW)
+read.csv("D:/Projects/Blocks/Data/Epifauna_Wet.csv") %>%
+filter(ID != "??") %>%
+mutate(Wet_mg = Wet_g * 1000, Treatment = case_when(startsWith(Treatment, "G") == T ~ "SG",
+                                                    startsWith(Treatment, "O") == T ~ "OY",
+                                                    startsWith(Treatment, "C") == T ~ "CB")) %>%
+ggplot(aes(x = Treatment, y = Wet_mg, fill = Class)) +
+  geom_boxplot(outlier.shape = NA, position = position_dodge(1)) +
+  geom_jitter(position = position_dodge(1)) +
+  facet_wrap(~Site) +
+  scale_fill_discrete(labels = c("Amphipods", "Mollusks")) +
+  theme_classic() +
+  labs(fill = "Group")
