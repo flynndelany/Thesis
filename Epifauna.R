a1cd7b58f64bcbad14d653a1ef3a7289e1a07450
flynndelany <flynndelany@gmail.com>
2023-03-16 12:56:04 -0400

    Amphipod only graph

--- Epifauna.R
+++ Epifauna.R
@@ -14,10 +14,23 @@ ggplot(aes(x = Treatment, y = Wet_g, fill = Class)) +
   theme_classic() +
   labs(fill = "Group")
 
+### Just Amphipods
+read.csv("D:/Projects/Blocks/Data/Epifauna_Wet.csv") %>%
+  filter(Class == "Amp") %>%
+  mutate(Survey = as.ordered(Survey), Wet_mg = Wet_g * 1000, Treatment = case_when(startsWith(Treatment, "G") == T ~ "SG",
+                                                      startsWith(Treatment, "O") == T ~ "OY",
+                                                      startsWith(Treatment, "C") == T ~ "CB")) %>%
+  ggplot(aes(x = Treatment, y = Wet_g, fill = Survey)) +
+  geom_boxplot(outlier.shape = NA, position = position_dodge(1)) +
+  geom_point(position = position_dodge(1)) +
+  geom_vline(xintercept = c(1.5, 2.5, 3.59)) +
+  facet_wrap(~Site) +
+  theme_classic()
+
 ## Epiphytes
 
 read.csv("D:/Projects/Blocks/Data/Epiphytes.csv") %>%
-  mutate(Survey = as.ordered(Survey) ,Dry_mg = Dry_g * 1000, Treatment = case_when(startsWith(Block, "G") == T ~ "SG",
+  mutate(Survey = as.ordered(Survey), Dry_mg = Dry_g * 1000, Treatment = case_when(startsWith(Block, "G") == T ~ "SG",
                                                                                   startsWith(Block, "O") == T ~ "OY",
                                                                                   startsWith(Block, "C") == T ~ "CB")) %>%
   ggplot(aes(x = Treatment, y = Dry_g)) +
